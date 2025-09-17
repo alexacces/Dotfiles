bd3b36bbae603bbe7cb0001e6aaa1082753b6ec7
xexobi <fadlidev99@gmail.com>
2025-09-17 11:30:23 +0700

    back on boot params

--- modules/systemConfig/systemBoot.nix
+++ modules/systemConfig/systemBoot.nix
@@ -2,6 +2,10 @@
   ...
 }:
 {
+  boot.kernelParams = [
+    "quiet"
+    "video=1366x768"
+  ];
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
