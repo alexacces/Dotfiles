af1af5691afa0878fc74b155bb53bda74b687180
xexobi <fadlidev99@gmail.com>
2025-09-19 12:38:54 +0700

    rules on awesome wm

--- modules/dotConfig/awesome/rc.lua
+++ modules/dotConfig/awesome/rc.lua
@@ -521,8 +521,9 @@ awful.rules.rules = {
 	{ rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = false } },
 
 	-- Set Firefox to always map on the tag named "2" on screen 1.
-	-- { rule = { class = "Firefox" },
-	--   properties = { screen = 1, tag = "2" } },
+	--
+	{ rule = { class = "firefox" }, properties = { tag = "2" } },
+	{ rule = { class = "ghostty" }, properties = { tag = "1" } },
 }
 -- }}}
 
