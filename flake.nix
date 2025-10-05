c97740467f0381f6a92ae287d1d1ff5782de2443
xexobi <fadlidev99@gmail.com>
2025-10-05 11:40:03 +0700

    add miror tsing hua

--- flake.nix
+++ flake.nix
@@ -1,11 +1,17 @@
 {
   description = "My Home Manager config";
+nixConfig = {subtituters = 
+[ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
+};
+
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
+
 
   outputs =
     {
@@ -15,6 +21,8 @@
       ...
     }:
     let
+lib = nixpkgs.lib;
+	hostname = "ammv";
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
@@ -24,6 +32,12 @@
       homeDirectory = "/home/ammv/";
     in
     {
+nixosConfigurations.${hostname} = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+      };
       homeConfigurations.ammv = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
