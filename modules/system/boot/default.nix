ac9a14dec274a8f0da663af4ee7394f88fd5419f
xexobi <fadlidev99@gmail.com>
2025-10-14 15:39:24 +0700

    udpate

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -4,6 +4,7 @@
     "quiet"
     "video=1366x768"
   ];
+  boot.loader.grub.useOSProber = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
